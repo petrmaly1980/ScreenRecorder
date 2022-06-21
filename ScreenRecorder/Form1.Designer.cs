6c1fc8468c2deea6a25815c0229f61b468045a55
Petr Maly <peter.maly1980@gmail.com>
2022-06-21 10:45:54 +0200

    Add project files.

---
+++ ScreenRecorder/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ScreenRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.comboBoxFrameRate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxQuality = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(103, 54);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(95, 30);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start Recording";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(204, 54);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(95, 30);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop recording";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(14, 63);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(26, 13);
+            this.labelTime.TabIndex = 2;
+            this.labelTime.Text = "time";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // comboBoxFrameRate
+            // 
+            this.comboBoxFrameRate.FormattingEnabled = true;
+            this.comboBoxFrameRate.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30"});
+            this.comboBoxFrameRate.Location = new System.Drawing.Point(103, 27);
+            this.comboBoxFrameRate.Name = "comboBoxFrameRate";
+            this.comboBoxFrameRate.Size = new System.Drawing.Size(95, 21);
+            this.comboBoxFrameRate.TabIndex = 3;
+            this.comboBoxFrameRate.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FrameRate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Compress";
+            // 
+            // comboBoxQuality
+            // 
+            this.comboBoxQuality.FormattingEnabled = true;
+            this.comboBoxQuality.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100"});
+            this.comboBoxQuality.Location = new System.Drawing.Point(204, 27);
+            this.comboBoxQuality.Name = "comboBoxQuality";
+            this.comboBoxQuality.Size = new System.Drawing.Size(95, 21);
+            this.comboBoxQuality.TabIndex = 6;
+            this.comboBoxQuality.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 96);
+            this.Controls.Add(this.comboBoxQuality);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFrameRate);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Screen Recorder by Petr Malý 2022";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ComboBox comboBoxFrameRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxQuality;
+    }
+}
+
---
+++ ScreenRecorder/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Captura;
+
+namespace ScreenRecorder
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Stopwatch stopwatch = new Stopwatch();
+        private void UpdateTime()
+        {
+            labelTime.Text = GetTimeString(stopwatch.Elapsed);
+        }
+        private string GetTimeString(TimeSpan elapsed)
+        {
+            string result = string.Empty;
+            result = string.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+            elapsed.Hours,
+            elapsed.Minutes,
+            elapsed.Seconds,
+            elapsed.Milliseconds);
+            
+            return result;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateTime();
+            buttonStop.Enabled = false;
+            buttonStart.BackColor = Color.Green;
+            buttonStop.BackColor = Color.White;
+        }
+        public Recorder rec;
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Text = "Recording !";
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            buttonStart.BackColor = Color.Red;
+            buttonStop.BackColor = Color.Green;
+            string now = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH'.'mm'.'ss");
+            int fr = int.Parse(comboBoxFrameRate.Text);
+            int quality = int.Parse(comboBoxQuality.Text);
+            //Recorder recorder = new Recorder(new RecorderParams("out.avi", 10, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, 70));
+            rec = new Recorder(new RecorderParams("record_"+now+".avi", fr, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, quality));
+            //rec = recorder;
+            stopwatch.Start();
+            timer.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonStart.Text = "Start Recording";
+            buttonStart.BackColor = Color.Green;
+            buttonStop.BackColor= Color.White;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            stopwatch.Stop();
+            rec.Dispose();
+            timer.Enabled = false;
+            stopwatch.Reset();
+            UpdateTime();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+    }
+}
